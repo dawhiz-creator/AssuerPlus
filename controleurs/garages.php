a5a8cc931f15c5fff991d2a03ea5c627aefe4d88
dawhiz <dawhiz@live.fr>
2023-04-13 23:32:23 +0200

    renew garages search page

--- controleurs/garages.php
+++ controleurs/garages.php
@@ -14,17 +14,40 @@ class garages extends Controleur
 
 $title='garages';
 $content = '
-<div>
-    <button type="button" id="search" class="btn btn-primary">Rechercher l\'adresse</button>
-    <div class="form-group">
-        <label for="address">Adresse</label>
-        <input type="text" class="form-control" id="address">
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-md-6 mx-auto">
+            <h1 class="text-center mb-4">Rechercher une adresse</h1>
+            <div class="form-group">
+
+                    <div class="row mt-5">
+            <div class="col-md-8 mx-auto text-center">
+
+                <p>Vous pouvez saisir une adresse complète ou partielle dans le champ de saisie, puis cliquer sur le
+                    bouton "Rechercher". Les résultats s\'afficheront dans la zone de résultats ci-dessous.</p>
+            </div>
+        </div>
+        
+                <label for="address">Adresse</label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="address" placeholder="Entrez une adresse...">
+                    <div class="input-group-append">
+                        <button type="button" id="search" class="btn btn-primary">Rechercher</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Résultats</h5>
+                    <ul id="results" class="list-group">
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
-    <ul id="results"></ul>
 </div>
 
 <script>
-    // Création d\'un site requêtant des API géographiques
     const url = new URL(\'https://api-adresse.data.gouv.fr/search\')
 
     function searchAddress() {
@@ -43,7 +66,7 @@ $content = '
             .then((data) => {
                 fillResults(data)
             })
-            .catch((error) => console.error(error)) //Traitement de l\'erreur dans l\'appel
+            .catch((error) => console.error(error))
     }
 
     function fillResults(data) {
@@ -52,18 +75,28 @@ $content = '
         if (undefined !== data.features) {
             data.features.forEach(function (element) {
                 let li = document.createElement(\'li\')
+                li.className = "list-group-item"
                 li.appendChild(document.createTextNode(element.properties.label))
                 list.appendChild(li)
             });
+        } else {
+            let li = document.createElement(\'li\')
+            li.className = "list-group-item"
+            li.appendChild(document.createTextNode("Aucun résultat trouvé."))
+            list.appendChild(li)
         }
     }
 
     document.getElementById("search").addEventListener("click", (event) => {
-        event.preventDefault() // évite que la page ne recharge
+        event.preventDefault()
         searchAddress();
     });
 
 </script>
+
+
+
+
 
 ';
 include("vues/layout.php");
